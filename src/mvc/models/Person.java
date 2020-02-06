3c8a2d7c2d21c21c9ac7503cb4b0c9cd022d2d63
mohan290 <54265513+mohan290@users.noreply.github.com>
2020-02-06 15:17:19 +0530

    Add files via upload

---
+++ src/mvc/models/Person.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class Person {
+    private String name;
+    private String mob;
+    private String email;
+    private String city;
+    private String nation;
+    
+    
+   /* public User(){
+        x=0;
+    }
+    
+    public User(int x){
+        this.x=x;
+    }
+    
+    public void incX(){
+        x++;
+    }
+    
+    public void decX(){
+        x--;
+    }
+    public int getX(){
+        return x;
+    }*/
+    public String getName(){
+        return this.name; 
+   }
+    
+    public String getMob(){
+        return this.mob; 
+   }
+    
+    public String getEmail(){
+        return this.email; 
+   }
+    public String getCity(){
+        return this.city;
+    }
+    public String getNation(){
+        return this.nation;
+    }
+   
+    public void setData(String name, String mob,String email,String city,String nation){
+        this.name = name;
+        this.mob = mob;
+        this.email = email;
+        this.city = city;
+        this.nation = nation;
+        
+    }
+    
+ 
+
+}
